da9291c284f9af68b6d661d7c8eca5b6885e29a7
JohnPYar <johnpyar@gmail.com>
2022-01-27 23:51:06 +0300

    парсинг изображений для категорий

--- dom24_parser/dom24_parser/spiders/category_img_parser.py
+++ dom24_parser/dom24_parser/spiders/category_img_parser.py
@@ -3,10 +3,12 @@ import scrapy
 
 class CategoryImgScraper(scrapy.Spider):
     name = 'category_img'
-    start_urls = ['https://pvhmarket.ru/catalog/paneli_plastikovye/paneli_centurion/']
+    start_urls = [''https://pvhmarket.ru/catalog/paneli_plastikovye/paneli_centurion/'']
 
     def parse(self, response):
+        categories_names = response.css('.item .name .dark_link::text').getall()
         categories_images = response.css("div.img.shine a img::attr(src)")
+
         for image in categories_images:
             yield {
                 'Category_img': f"https:{image.get()}"
