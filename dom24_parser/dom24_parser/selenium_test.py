500843847f795fabccf929ca156682b8ce998237
JohnPYar <johnpyar@gmail.com>
2022-03-11 20:57:05 +0300

    тест selenium

--- dom24_parser/dom24_parser/selenium_test.py
+++ dom24_parser/dom24_parser/selenium_test.py
@@ -2,4 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+url = 'https://www.panda-panel.ru/catalog/pvkh_paneli/dlya_vann/parizh.html'
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
