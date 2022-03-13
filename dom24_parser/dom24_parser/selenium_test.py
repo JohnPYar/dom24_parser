9f744f599201bc0fe90214e17074c41b6f970544
Evgeny Pavlov <e_a_pavlov@mail.ru>
2022-03-13 23:06:07 +0300

    selenium тестирую

--- dom24_parser/dom24_parser/selenium_test.py
+++ dom24_parser/dom24_parser/selenium_test.py
@@ -1,7 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-url = 'https://www.panda-panel.ru/catalog/pvkh_paneli/dlya_vann/parizh.html'
+import scrapy
+import html
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.panda-panel.ru/catalog/pvkh_paneli/dlya_vann/mechta.html'
+
+options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+
+driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+driver.get(url)
+
+sku = driver.find_element(By.LINK_TEXT, '02510')
+
+print(sku)
+
+sku.click()
+
+WebDriverWait(driver, timeout=10)
+
+title = driver.find_element(By.XPATH, '//h1')
+
+# soup = bs(page_source, 'html.parser')
+# title = soup.find(class_='changeName').text
+print(title.text)
