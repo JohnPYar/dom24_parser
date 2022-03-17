aa1c96fdfad9c0d066f55acbb16d93439f6df4e0
Evgeny Pavlov <e_a_pavlov@mail.ru>
2022-03-17 21:54:24 +0300

    вытаскиваем Панда панели с помощью selenium

--- dom24_parser/dom24_parser/spiders/panda_paneli.py
+++ dom24_parser/dom24_parser/spiders/panda_paneli.py
@@ -24,14 +24,14 @@ class Dom24PandaPaneliSpider(scrapy.Spider):
             yield response.follow(product_link, self.parse_product, cb_kwargs=dict(category_name=category_name))
 
     #     проверяем на наличие пагинации на странице товаров в категории
-        next_page = response.css('div.nums li.flex-nav-next a::attr(href)').get()
+        next_page = response.css('li.bx-pag-next a::attr(href)').get()
         if next_page is not None:
             yield response.follow(next_page, self.parse_category, cb_kwargs=dict(category_name=category_name))
 
     def parse_product (self, response, category_name):
         # получаем характеристика товара в виде аттрибутов
         attributes = ''
-        attrs = response.css('div.propertyTable')
+        attrs = response.css('div.propertyList')
         # счетчик для определения последнего элемента массива
         counter = 0
         for attr in attrs:
@@ -40,19 +40,25 @@ class Dom24PandaPaneliSpider(scrapy.Spider):
             value = attr.css('div.propertyValue::text').get().strip()
             attributes += f"{name}---{value}"
             #     если элемент не последний добавляем разделитель
-            if counter != len(attrs):
+            if counter != (len(attrs) - 1):
                 attributes += "|"
 
-        yield {
-            # 'product_id': product_id,
-            'Category': category_name,
-            'Name': response.css('h1.changeName::text').get().strip(),
-            'Title': response.css('h1.changeName::text').get().strip(),
-            # 'Image': f"https://www.panda-panel.ru{response.css('div.pictureSlider img::attr(src)').get()}",
-            'Image': f"https://www.panda-panel.ru{response.css('div.pictureSlider a::attr(href)').get()}",
-            'Price': response.css('div#elementTools span.priceVal::text').get().replace(' руб.', '').strip(),
-            'Model': response.css('h1.changeName::text').get().strip(),
-            # 'Description': description_escape
-            'Description': response.css('div.changeShortDescription::text').get().strip(),
-            'Properties': attributes
-        }
+        # проверяем наличие артикулов(вариантов) товара, если есть, то парсим как отдельные товары через selenium, модель будет у всех одна, по главному заголовку
+        skus_amount = len(response.css('ul.elementSkuPropertyList'))
+        if skus_amount > 1:
+
+
+
+        # yield {
+        #     # 'product_id': product_id,
+        #     'Category': category_name,
+        #     'Name': response.css('h1.changeName::text').get().strip(),
+        #     'Title': response.css('h1.changeName::text').get().strip(),
+        #     # 'Image': f"https://www.panda-panel.ru{response.css('div.pictureSlider img::attr(src)').get()}",
+        #     'Image': f"https://www.panda-panel.ru{response.css('div.pictureSlider a::attr(href)').get()}",
+        #     'Price': response.css('div#elementTools span.priceVal::text').get().replace(' руб.', '').strip(),
+        #     'Model': response.css('h1.changeName::text').get().strip(),
+        #     # 'Description': description_escape
+        #     'Description': response.css('div.changeShortDescription::text').get().strip(),
+        #     'Properties': attributes
+        # }
