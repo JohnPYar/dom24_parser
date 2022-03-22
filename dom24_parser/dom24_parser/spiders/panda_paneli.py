ce3ec668bc033bbf867ed9eb1f0d3f968abfa73b
JohnPYar <johnpyar@gmail.com>
2022-03-23 00:20:16 +0300

    парсим панели Панда scrapy + scrapy-playwright

--- dom24_parser/dom24_parser/spiders/panda_paneli.py
+++ dom24_parser/dom24_parser/spiders/panda_paneli.py
@@ -26,44 +26,44 @@ class Dom24PandaPaneliSpider(scrapy.Spider):
         yield scrapy.Request(start_url, meta={"playwright": True})
 
     def parse(self, response):
-        yield {"url": response.url}
-        # categories = response.css('a.catalog-section-list-link')
-        # for category in categories:
-        #     category_name = category.css('span::text').get().strip()
-        #     category_url = category.attrib['href']
-        #     yield response.follow(category_url, self.parse_category, cb_kwargs=dict(category_name=category_name))
+        # yield {"url": response.url}
+        categories = response.css('a.catalog-section-list-link')
+        for category in categories:
+            category_name = category.css('span::text').get().strip()
+            category_url = category.attrib['href']
+            yield response.follow(category_url, self.parse_category, cb_kwargs=dict(category_name=category_name), meta={"playwright": True})
 
-    # def parse_category(self, response, category_name):
-    #     products = response.css('div.productColText')
-    #     for product in products:
-    #         product_link = product.css('a.name::attr(href)').get()
-    #         yield response.follow(product_link, self.parse_product, cb_kwargs=dict(category_name=category_name))
-    #
-    # #     проверяем на наличие пагинации на странице товаров в категории
-    #     next_page = response.css('li.bx-pag-next a::attr(href)').get()
-    #     if next_page is not None:
-    #         yield response.follow(next_page, self.parse_category, cb_kwargs=dict(category_name=category_name))
-    #
-    # def parse_product(self, response, category_name):
-    #     # получаем характеристика товара в виде аттрибутов
-    #     attributes = ''
-    #     attrs = response.css('div.propertyList')
-    #     # счетчик для определения последнего элемента массива
-    #     counter = 0
-    #     for attr in attrs:
-    #         counter += 1
-    #         name = attr.css('div.propertyName::text').get().strip()
-    #         value = attr.css('div.propertyValue::text').get().strip()
-    #         attributes += f"{name}---{value}"
-    #         #     если элемент не последний добавляем разделитель
-    #         if counter != (len(attrs) - 1):
-    #             attributes += "|"
-    #
-    #     # проверяем наличие артикулов(вариантов) товара,
-    #     # если есть, то парсим как отдельные товары через selenium, модель будет у всех одна, по главному заголовку
-    #     skus_amount = len(response.css('ul.elementSkuPropertyList'))
-    #     if skus_amount > 1:
-    #         pass
+    def parse_category(self, response, category_name):
+        products = response.css('div.productColText')
+        for product in products:
+            product_link = product.css('a.name::attr(href)').get()
+            yield response.follow(product_link, self.parse_product, cb_kwargs=dict(category_name=category_name))
+
+    #     проверяем на наличие пагинации на странице товаров в категории
+        next_page = response.css('li.bx-pag-next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse_category, cb_kwargs=dict(category_name=category_name))
+
+    def parse_product(self, response, category_name):
+        # получаем характеристика товара в виде аттрибутов
+        attributes = ''
+        attrs = response.css('div.propertyList')
+        # счетчик для определения последнего элемента массива
+        counter = 0
+        for attr in attrs:
+            counter += 1
+            name = attr.css('div.propertyName::text').get().strip()
+            value = attr.css('div.propertyValue::text').get().strip()
+            attributes += f"{name}---{value}"
+            #     если элемент не последний добавляем разделитель
+            if counter != (len(attrs) - 1):
+                attributes += "|"
+
+        # проверяем наличие артикулов(вариантов) товара,
+        # если есть, то парсим как отдельные товары через selenium, модель будет у всех одна, по главному заголовку
+        skus_amount = len(response.css('ul.elementSkuPropertyList'))
+        if skus_amount > 1:
+            pass
 
         # yield {
         #     # 'product_id': product_id,
