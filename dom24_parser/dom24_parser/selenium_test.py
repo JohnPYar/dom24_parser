0e19c08480e5f60d37afb566264064dbeff11aa0
Evgeny Pavlov <e_a_pavlov@mail.ru>
2022-04-14 18:12:20 +0300

    selenium is worked for multi clicks!!!!

--- dom24_parser/dom24_parser/selenium_test.py
+++ dom24_parser/dom24_parser/selenium_test.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 import scrapy
 import html
@@ -18,16 +21,44 @@ driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager()
 
 driver.get(url)
 
-sku = driver.find_element(By.LINK_TEXT, '02510')
+skus = driver.find_elements(By.CLASS_NAME, 'skuPropertyItemLink')
 
-print(sku)
+# print(skus)
 
-sku.click()
+for sku in skus:
+    # driver.implicitly_wait(1000)
+    # print(sku)
 
-WebDriverWait(driver, timeout=10)
+    sku.click()
 
-title = driver.find_element(By.XPATH, '//h1')
+    # driver.implicitly_wait(1000)
+    time.sleep(2)
 
-# soup = bs(page_source, 'html.parser')
-# title = soup.find(class_='changeName').text
-print(title.text)
+    # WebDriverWait(driver, timeout=1000)
+
+    title = driver.find_element(By.CSS_SELECTOR, 'h1.changeName')
+    # title = driver.find_element(By.XPATH, '//h1')
+    # title = WebDriverWait(driver, 10000).until(lambda x: x.find_element(By.XPATH, '//h1'))
+
+    # soup = bs(page_source, 'html.parser')
+    # title = soup.find(class_='changeName').text
+    print(title.text)
+
+# sku_list = []
+#
+# for i in range(0, len(skus)):
+#     # print(skus[i])
+#     skus[i].click()
+#     time.sleep(5)
+#     driver.implicitly_wait(100)
+#     title = driver.find_element(By.CSS_SELECTOR, 'h1.changeName')
+#     sku_list.append(title.text)
+#     # print(title.text)
+# print(sku_list)
+# skus[2].click()
+# # WebDriverWait(driver, timeout=10000)
+# driver.implicitly_wait(10)
+# # title = driver.find_element(By.XPATH, '//h1')
+# title = driver.find_element(By.CSS_SELECTOR, 'h1.changeName')
+# # title = WebDriverWait(driver, 10000).until(lambda x: x.find_element(By.XPATH, '//h1'))
+# print(title.text)
